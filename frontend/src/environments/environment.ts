ae2e0618e955cee5a34b1c6bfa2855a7dbf1e690
FausVillegas <131504873+FausVillegas@users.noreply.github.com>
2024-07-30 20:35:55 -0300

    Update and rename environments.ts to environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
-export const env = {
-    production: false,
+export const environment = {
+    production: true,
     apiUrl: 'https://websattva-backend.onrender.com',
     secretKeyForToken: 'secretfortoken12052004'
-  };
+};
