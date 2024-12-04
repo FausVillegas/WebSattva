b6f5d7746b70facbfcceea5f45586412f7168219
Fausto Villegas <fvillegas@sinergiasoluciones.com.ar>
2024-12-04 08:34:08 -0300

    Se agrega la lista de días semanales y hora de las clases dentro de sus tarjetas de detalles  y en la lista de Mis clases y eventos

--- frontend/src/app/components/user-registrations/user-registrations.component.ts
+++ frontend/src/app/components/user-registrations/user-registrations.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { ClassEventDetailsComponent } from '../class-event/class-event-details/class-event-details.component';
 import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'src/environments/environment';
+import { ClassService } from 'src/app/services/class.service';
 
 @Component({
   selector: 'app-user-registrations',
@@ -12,14 +13,22 @@ import { environment } from 'src/environments/environment';
 export class UserRegistrationsComponent implements OnInit{
   events: any[] = [];
   classes: any[] = [];
+  classesSchedules: any[] = [];
+
   apiUrl = environment.apiUrl;
 
-  constructor(private authService: AuthService, private dialog: MatDialog) {}
+  constructor(private authService: AuthService, private dialog: MatDialog, private classService: ClassService) {}
 
   ngOnInit(): void {
     this.authService.getUserRegistrations().subscribe((data) => {
       this.events = data.events;
       this.classes = data.classes;
+      this.classes.forEach(element => {
+        this.classService.getClassById(Number(element.id)).subscribe(classData => {
+          console.log(classData);
+          this.classesSchedules[element.id] = classData.classSchedules;
+        });
+      });
     });
   }
 
